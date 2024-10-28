05bf632525136451a4ceb68d66d876eb8cdedb4e
boracingoz26 <boracingoz@gmail.com>
2024-10-28 06:53:51 +0300

    Added character controller and implemented jump mechanics.

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public CharacterController characterController;
+
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
-
-    private Rigidbody _rb;
+    [SerializeField] private float _gravity = 10;
+    
     private Vector3 _moveDirection;
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float yStore = _moveDirection.y;
         _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical"));
-        transform.position = transform.position + (_moveDirection * Time.deltaTime * _moveSpeed);
+        _moveDirection = _moveDirection * _moveSpeed;
+        _moveDirection.y = yStore;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _moveDirection.y = _jumpForce;
+        }
+
+        _moveDirection.y += Physics.gravity.y * Time.deltaTime * _gravity;
+
+        //transform.position = transform.position + (_moveDirection * Time.deltaTime * _moveSpeed);
+
+        characterController.Move(_moveDirection * Time.deltaTime);
     }
 }
